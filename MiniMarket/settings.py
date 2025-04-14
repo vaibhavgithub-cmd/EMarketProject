6f40b40dc0a3c1331a569b516b19e3a639ec4662
vaibhavgithub-cmd <king96vaibhav@gmail.com>
2025-04-14 21:22:21 +0530

    commit2

--- MiniMarket/settings.py
+++ MiniMarket/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-e99h)%(el!&qitp7ve5=kuor_bul&tjbj*v1tbk9d0oymgwp%c
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [".vercel.app"]
 
 
 # Application definition
